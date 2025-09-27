bfdc5078305e48c737bec4127ae54f9a385f6f69
Roman Sotnikov <axeman.ofic@gmail.com>
2025-09-27 22:06:41 +0300

    ci: update ci

--- noxfile.py
+++ noxfile.py
@@ -5,14 +5,19 @@ import nox
 if TYPE_CHECKING:
     from nox.sessions import Session
 
+nox.options.default_venv_backend = "uv"
+
 
 @nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
 def tests(session: "Session") -> None:
+    """Run all tests."""
     session.install("pytest")
     session.run("pytest", "-v", "tests")
 
 
 @nox.session
 def lint(session: "Session") -> None:
-    session.install("ruff")
-    session.run("ruff", "check", "clite")
+    """Run all linters."""
+    session.install("ruff", "mypy")
+    # session.run("ruff", "check", ".")
+    session.run("mypy", ".", "-v")

8973af8c7bfba609fd04a9e4d464d7bba081487f
Roman Sotnikov <axeman.ofic@gmail.com>
2026-04-01 20:28:50 +0300

    feat: add support rich

--- clite/helper.py
+++ clite/helper.py
@@ -1,5 +1,10 @@
 from typing import TYPE_CHECKING
 
+from rich.console import Console
+from rich.containers import Lines
+from rich.text import Text
+
+from clite.rich_utils import _to_ansi
 from clite.utils import echo
 
 if TYPE_CHECKING:
@@ -45,6 +50,69 @@ class Helper:
                     echo(f"{param.name}/{param.short_name} - {param.annotation} - {param.name}")
                 else:
                     echo(f"{param.name} - {param.annotation} - {param.name}")
+
+    def create_help_clite(self, instance: "Clite") -> None:
+        """Create help for clite.
+
+        :param instance: clite instance
+        :return: None
+        """
+        echo(f"{instance.name} - {instance.description}\n")
+        echo(f"Usage: {instance.name} [OPTIONS] <COMMAND>\n")
+        echo("Commands:")
+        for cmd in instance.commands.values():
+            echo(f"{cmd.name} - {cmd.description}")
+
+
+class RichHelper(Helper):
+    def __init__(self) -> None:
+        self.console = Console(color_system="auto", force_terminal=True, markup=True)
+        super().__init__()
+
+    def create_help_command(
+        self,
+        instance: "Clite",
+        cmd: "Command",
+        params: "Mapping[str, ParameterInfo]",
+    ) -> None:
+        """Create help for command.
+
+        :param instance: clite instance
+        :param cmd: command
+        :return: None
+        """
+        text = Lines()
+        text.append(Text.from_markup(f"[bold]{cmd.name}[/bold] - {cmd.description}\n"))
+        text.append(Text.from_markup(f"[bold]Usage[/]: {instance.name} {cmd.name} [OPTIONS]\n"))
+
+        from rich.table import Table
+
+        grid = Table.grid(padding=(0, 3, 0, 0))
+
+        for _, param in params.items():
+            if param.is_optional:
+                grid.add_row(
+                    f"{param.name}/{param.short_name}",
+                    f"{param.annotation}",
+                    f"{param.name} - \\[default: {param.value}]",
+                )
+                # text.append(
+                #     Text.from_markup(
+                #         f"{param.name}/{param.short_name} - {param.annotation} - {param.name} - \\[default: {param.value}]",
+                #     ),
+                # )
+            else:
+                grid.add_row(
+                    f"{param.name}",
+                    f"{param.annotation}",
+                    f"{param.name}",
+                )
+                # text.append(
+                #     Text.from_markup(f"{param.name} - {param.annotation} - {param.name}"),
+                # )
+        _ansi_text = _to_ansi(self.console, text)
+        echo(_ansi_text.rstrip("\n"))
+        echo(_to_ansi(self.console, grid))
 
     def create_help_clite(self, instance: "Clite") -> None:
         """Create help for clite.
